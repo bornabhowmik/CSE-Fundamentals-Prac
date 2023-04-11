56408616d391863c9b6cf38e2a46ef901e1c7e74
bornabhowmik <bornabhowmik4@gmail.com>
2023-04-11 10:49:46 +0600

    Divisible & Divisible-1 Problem solved

--- Shopping.c
+++ Shopping.c
@@ -7,19 +7,18 @@ int main()
 
     if(tk>1000)
     {
-        printf("I will buy a Punjabi\n");
+        printf("I will buy Punjabi\n");
         tk-=1000;
-    }
-    else if(tk>=500)
-    {
-        printf("I will buy new shoes\n");
-        printf("Allisa will buy shoes\n");
+        if(tk>=500)
+        {
+            printf("I will buy new shoes\n");
+            printf("Alisa will buy new shoes\n");
+        }
     }
     else
     {
-        printf("Bad Luck!\n");
-        return 0;
+        printf("Bad luck!\n");
     }
-
     
+    return 0;
 }
