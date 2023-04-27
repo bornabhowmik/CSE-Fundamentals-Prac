40b143a4b95906ee743c7376bc72b6170c253166
bornabhowmik <bornabhowmik4@gmail.com>
2023-04-28 03:27:23 +0600

    while.c code added

--- Module 3/do_while.c
+++ Module 3/do_while.c
@@ -2,12 +2,13 @@
 int main()
 {
     int i;
-    // while
+    // do while
     i=1;
-    while(i<=5)
+    do
     {
         printf("%d\n",i);
         i=i+1;
     }
-    return 0;
+     while (i<=5);
+     return 0;
 }
