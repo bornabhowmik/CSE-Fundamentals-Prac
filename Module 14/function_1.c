7bf0dc3eb10c06c1170debd922320af4fdad9577
bornabhowmik <bornabhowmik4@gmail.com>
2023-05-18 23:36:49 +0600

    function_1.c code added

---
+++ Module 14/function_1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int sum(int x,int y)
+
+{
+    int sum=x+y;
+    return sum;
+}
+
+int main()
+
+{
+    int x,y;
+    scanf("%d %d",&x,&y);
+    printf("%d",sum(x,y));
+    
+    return 0;
+}
